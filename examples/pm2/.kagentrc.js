20707ea3e0237fd7b4e4c36414aae3812816fa7f
imhype <543717080@qq.com>
2018-12-03 10:57:55 +0800

    fix: use 127.0.0.1

--- examples/pm2/.kagentrc.js
+++ examples/pm2/.kagentrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "server": "http://localhost:7009",
+  "server": "http://127.0.0.1:7009",
   "appid": "1",
   "secret": "c1e71150-f47f-11e8-be01-9da91475be14",
   "cluster": "localjd"

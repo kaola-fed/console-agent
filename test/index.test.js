dfcbe3530542e61066d3fd3a0ea47ee6ee495199
imhype <543717080@qq.com>
2019-01-08 19:53:55 +0800

    feat: Agent

---
+++ test/index.test.js
@@ -0,0 +1,13 @@
+// const assert = require('power-assert');
+describe('Agent', function() {
+  before(async function() {
+        
+  })
+
+  it('should be ok', async function() {
+    
+  });
+
+  after(function() {
+  })
+});
